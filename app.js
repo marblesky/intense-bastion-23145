7569d1d4dd227b7da432dddec627ac5c90aeee15
totop\imimamo <marble@clippu.com>
2017-05-13 09:47:00 +0900

    routes/ndex_postの削除

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-//var index_post= require('./routes/index_post');
 
 var app = express();
 
@@ -25,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-// app.post('/',index_post.index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
