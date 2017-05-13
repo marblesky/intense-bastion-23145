080476f72e5aa5504fc8da238277fa127a859347
totop\imimamo <marble@clippu.com>
2017-05-13 10:38:04 +0900

    クッキーを追加

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    msg: 'こんにちは！'
- });
+  var cookie =req.cookies;
+  if(cookie==undefined){
+    var str = '？？'
+  }else{
+    var str=cookie.str
+  }
+  res.render('index',
+      {
+        title: 'Express',
+        msg: "こんにちは、" + str + "さん！" ,
+        input: str
+      }
+  );
 });
 
+
 router.post('/', function(req, res, next) {
+var cookie =req.cookieParser;
+  if(cookie==undefined){
+    var str = '？？'
+  }else{
+    var str=cookie.str
+  }
 var str = req.body['text1'];
+res.cookie("str",str,{maxAge:600000});
     res.render('index',
         {
             title: 'Express',
