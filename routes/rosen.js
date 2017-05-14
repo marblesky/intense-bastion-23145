16f1e2c14e5c16bcc286a95397711d3f8a76b478
totop\imimamo <marble@clippu.com>
2017-05-15 07:00:16 +0900

    rosen.jsをherokuに対応
    スキーマの名称を環境によって変更するように変更

--- routes/rosen.js
+++ routes/rosen.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 var connection=require("../lib/mysqlConnection.js");
 
+switch (app.get('env')) {
+case 'dev':
+case 'stg':
+case 'prd':
+      var scm= 'bkn_master';
+break;
+case 'hrk':
+      var scm= 'heroku_9e23a2e7e50daa9';
+break;
+default:
+      var scm= 'bkn_master';
+break;
+}
+
 router.get('/', function(req, res, next) {
-  var query = 'SELECT * from bkn_master.沿線駅マスタ order by 沿線駅コード';
+  var query = 'SELECT * from ' + scm + '.沿線駅マスタ order by 沿線駅コード';
   connection.query(query, function(err, rows) {
     res.render('rosen', {
       title: '日本の駅一覧',
